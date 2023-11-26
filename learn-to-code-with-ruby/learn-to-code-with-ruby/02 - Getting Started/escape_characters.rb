70c0c182d88e7ec2dcc5aa9ee788bd3753e8b3eb
kirill-baudzei <kirillbaudzei@gmail.com>
2023-11-26 22:35:44 +0100

    added code to files

--- learn-to-code-with-ruby/learn-to-code-with-ruby/02 - Getting Started/escape_characters.rb
+++ learn-to-code-with-ruby/learn-to-code-with-ruby/02 - Getting Started/escape_characters.rb
@@ -1,8 +1,8 @@
-puts "some text\nmore text"
-puts "\tOnce upon a time"
+puts "some text\nmore text" # спец символ - разрыв строки
+puts "\tOnce upon a time" # делает отступ (пробел)
 
-puts "Juliet said \"Goodbye\" to Romeo"
-puts 'Juliet said \'Goodbye\' to Romeo'
+puts "Juliet said \"Goodbye\" to Romeo" # делает кавычки текстом 
+puts 'Juliet said \'Goodbye\' to Romeo' 
 
 puts "Juliet said 'Goodbye' to Romeo"
 puts 'Juliet said "Goodbye" to Romeo'
--- learn-to-code-with-ruby/learn-to-code-with-ruby/02 - Getting Started/the_print_method.rb
+++ learn-to-code-with-ruby/learn-to-code-with-ruby/02 - Getting Started/the_print_method.rb
@@ -1,2 +1,4 @@
+# отличия print от puts
+
 print "This random nonsense "
-print "will continue on the same line"
+print "will continue on the same line" # выводится все в одну строку , а puts всегда в новой строке
