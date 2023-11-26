982195eb2a654beb22181980a76e99c1d67a9a71
kirill-baudzei <kirillbaudzei@gmail.com>
2023-11-26 23:53:58 +0100

    added code to files

---
+++ learn-to-code-with-ruby/learn-to-code-with-ruby/03 - Variables/declaring_variables.rb
@@ -0,0 +1,24 @@
+age = 31
+puts age
+
+name = "Boris"
+last_name = "Danger"
+puts name
+puts last_name
+
+puts age + 7 # 31 + 7
+puts name + last_name
+
+age = 35
+puts age
+
+age_in_ten_years = age + 10 # 45
+puts age_in_ten_years
+
+age = age + 7 # 42
+puts age
+
+chameleon = 24
+chameleon = "Some random text"
+chameleon = 3.14
+puts chameleon # выводится последнее объявление
