ff37320f361242d0d10ff4cfccb35816ef4e6437
kirill-baudzei <kirillbaudzei@gmail.com>
2023-11-27 00:39:11 +0100

    added new\ code to files

---
+++ learn-to-code-with-ruby/learn-to-code-with-ruby/04 - Object Methods/intro_to_object_methods.rb
@@ -0,0 +1,18 @@
+# введение в методы 
+
+puts "Hello World".length # метод выводит кол-во символов в стринге - 11
+
+puts "hello world".upcase # сделает весь string капсом. выведет HELLO WORLD
+
+puts "TOTALLY DIFFERENT".downcase() # сделает все в lowercase - выведет totally different
+
+# () - в конце метода нужны для придания значений методу, если нужно. Если не нужно, то они ни на что не влияют. Но для красивого кода
+# их лучше убирать
+
+expressins = "TOTALLY DIFFERENT"
+puts expressins.downcase # сделает все в lowercase - выведет totally different. Аналогично записи выше
+
+
+
+
+
