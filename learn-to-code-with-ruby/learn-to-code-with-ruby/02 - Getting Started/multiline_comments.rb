6c79ad6fc1971449d7ce1d96935c13a8abf59ab5
kirill-baudzei <kirillbaudzei@gmail.com>
2023-11-26 22:45:12 +0100

    added code to files

---
+++ learn-to-code-with-ruby/learn-to-code-with-ruby/02 - Getting Started/multiline_comments.rb
@@ -0,0 +1,8 @@
+# есть второй варини записи комментария открыть через = begin и закрыть через =end - тогда все между ними будет комментами
+
+=begin
+Hello there!
+This is a multi-line comment
+Lots of fun code down below
+Have a blast!
+=end
