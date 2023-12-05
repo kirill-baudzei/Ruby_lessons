3ef7d657ce34584e687bb182b8a5c2208fc79e01
kirill-baudzei <kirillbaudzei@gmail.com>
2023-12-05 20:19:08 +0100

    added code

---
+++ learn-to-code-with-ruby/learn-to-code-with-ruby/12 - Arrays I/equality_and_inequality_operations_with_arrays.rb
@@ -0,0 +1,1 @@
+# equality_and_inequality_operations_with_arrays. Равенство и неравенство с массивами
---
+++ learn-to-code-with-ruby/learn-to-code-with-ruby/12 - Arrays I/the_empty__and_nil__methods.rb
@@ -0,0 +1,10 @@
+# методы empty? и nil?
+
+puts ["A", "B"].empty? # false
+puts [].empty? # true
+puts [].length == 0 # true
+
+puts
+
+puts [].nil? # false
+puts nil.nil? # true
