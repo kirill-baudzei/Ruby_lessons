b6fbcaa088d65dfe49bd099ac081f494c8e7396e
kirill-baudzei <kirillbaudzei@gmail.com>
2023-12-20 22:28:41 +0100

    added code examples

--- learn-to-code-with-ruby/learn-to-code-with-ruby/17 - Hashes/reference_problems_with_Hash.new.rb
+++ learn-to-code-with-ruby/learn-to-code-with-ruby/17 - Hashes/reference_problems_with_Hash.new.rb
@@ -1,10 +1,10 @@
 # what Ruby will give to you when a key does not exist
 # Руби отдает это значение, когда такого ключа нет
-team_members = Hash.new do |hash, key|
-  hash[key] = []
+team_members = Hash.new do |hash, key| # этот блок кода работает, только если в хэше нет такого ключа (key)
+  hash[key] = [] # по умолчанию значение - пустой массив
 end
   
-  p team_members["Buccaneers"] # []
+  p team_members["Buccaneers"] # [] - значение по умолчанию. Но ключ уже существует со значением по умолчанию
   p team_members
   
   team_members["Buccaneers"] << "Tom Brady" # ключу даем значение
